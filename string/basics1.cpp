7308812b25ca59b4fb68aafc845a4ecd0e64e327
haarshitaa <harshitacodes@gmail.com>
2025-02-13 02:21:23 +0530

    strings

---
+++ string/basics1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str; // Input a string (stops at whitespace)
+    cout << str << endl;
+    return 0;
+}
