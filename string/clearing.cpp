561e3dc7cd3647e727f9bd3737aa99581a6a806a
haarshitaa <harshitacodes@gmail.com>
2025-02-13 02:23:05 +0530

    strings

---
+++ string/clearing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s1 = "familyfhujsjhbsjhfbhsjfs";
+    s1.clear(); // Clear the string
+    cout << s1 << endl; // Output will be empty
+    return 0;
+}
